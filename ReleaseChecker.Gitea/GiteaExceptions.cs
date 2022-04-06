3605af4a8708a093e18dc73ec33b13c6acb00e4e
Дмитрий Худяков <skitles55@gmail.com>
2022-04-06 20:12:03 +0600

    Start creating Gitea library

---
+++ ReleaseChecker.Gitea/GiteaExceptions.cs
@@ -0,0 +1,26 @@
+﻿namespace ReleaseChecker.Gitea
+{
+    public class InvalidServerUrlException : Exception
+    {
+        public InvalidServerUrlException()
+        : base("Invalid Gitea server url.") { }
+    }
+
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        : base("Gitea Repo not found.") { }
+    }
+
+    public class RateLimitExceededException : Exception
+    {
+        public RateLimitExceededException()
+        : base("Gitea API rate limit exceeded.") { }
+    }
+
+    public class APICallFailedException : Exception
+    {
+        public APICallFailedException()
+        : base("Gitea API call failed.") { }
+    }
+}
---
+++ ReleaseChecker.Gitea/GiteaRelease.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ReleaseChecker.Gitea
+{
+    public class GiteaRelease : Release
+    {
+        [JsonPropertyName("tag_name")]
+        public new string Tag { get; set; } = "";
+
+        [JsonPropertyName("name")]
+        public new string Title { get; set; } = "";
+
+        [JsonPropertyName("body")]
+        public new string Description { get; set; } = "";
+
+        [JsonPropertyName("published_at")]
+        public new DateTime PublishDate { get; set; }
+
+        [JsonPropertyName("prerelease")]
+        public new bool PreRelease { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public new string Url { get; set; } = "";
+
+        [JsonPropertyName("assets")]
+        public new GiteaReleaseFile[]? Files { get; set; }
+    }
+}
